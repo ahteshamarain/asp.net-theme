11e522c05f5bdcc52d7b4ef496b93f44a76f2eb5
ahteshamarain <ahteshamarain3@gmail.com>
2024-09-05 13:17:39 +0500

    update

---
+++ theme/Controllers/UserController.cs
@@ -0,0 +1,39 @@
+﻿using System.Net;
+using System.Net.Mail;
+
+using Microsoft.AspNetCore.Mvc;
+
+namespace theme.Controllers
+{
+    public class UserController : Controller
+    {
+      
+
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Index(string email , string mess)
+        {
+            SmtpClient client = new SmtpClient("smtp.gmail.com", 587);
+            client.EnableSsl = true;
+            client.UseDefaultCredentials = false;
+
+            //ptmaptech@gmail.com    "ylkflsuagqvtshad"
+            client.Credentials = new NetworkCredential("ahteshamarain3@gmail.com", "vleemsjecrsslgaj");
+
+            MailMessage msg = new MailMessage(email, "ahteshamarain3@gmail.com");
+            msg.Subject = "Feedback";
+            msg.Body = mess;
+
+            // msg.Attachments.Add(new Attachment(PathToAttachment));
+            client.Send(msg);
+
+            ViewBag.message = "Mail sent successfully, Role assigned";
+
+            return View();
+        }
+    }
+}
